05c3e26fde53d9fe485697c246dcbefade2f1015
ffernandoss <fsantama@myuax.com>
2024-10-10 17:31:33 +0200

    creacion de tabla de datos y llenado del csv de tipo normal

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication{
+
+	private static DatosService valorDataService;
+
+	@Autowired
+	public DemoApplication(DatosService valorDataService) {
+		DemoApplication.valorDataService = valorDataService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		valorDataService.loadCSVToDatabase("demo/src/main/resources/distribucion_normal.csv");
 	}
 
 }
