63fb2010c52c6ced20e759626a7d26e8da412680
ffernandoss <fsantama@myuax.com>
2024-10-10 18:25:57 +0200

    continua hilos

--- demo/src/main/java/com/example/demo/DatosService.java
+++ demo/src/main/java/com/example/demo/DatosService.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +7,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Semaphore;
 
 @Service
 public class DatosService {
@@ -15,8 +16,9 @@ public class DatosService {
     @Autowired
     private DatosRepository datosRepository;
 
-    public List<Datos> getAllValores() {
+    private final Semaphore semaphore = new Semaphore(1);
 
+    public List<Datos> getAllValores() {
         return datosRepository.findAll();
     }
 
@@ -24,18 +26,26 @@ public class DatosService {
         return datosRepository.save(datos);
     }
 
-    public void loadCSVToDatabase(String csvFile) {
+    public void loadCSVToDatabase(String csvFile, ExecutorService executorService) {
         String line;
         String cvsSplitBy = ",";
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             while ((line = br.readLine()) != null) {
-
                 String[] conjunto = line.split(cvsSplitBy);
                 Datos datos = new Datos();
-
                 datos.setValue(String.valueOf(conjunto[0]));
-                datosRepository.save(datos);
+
+                executorService.execute(() -> {
+                    try {
+                        semaphore.acquire();
+                        Datos savedDatos = datosRepository.save(datos);
+                        System.out.println(Thread.currentThread().getName() + " - " + savedDatos.getValue());
+                        semaphore.release();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                });
             }
             System.out.println("finalizacion del llenado");
         } catch (IOException e) {
